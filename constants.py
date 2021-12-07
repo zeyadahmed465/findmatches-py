78ce16a9edda973da6ce2869b74959a1118cea59
Mohamed25885 <memo_868@yahoo.com>
2021-12-07 18:20:33 +0200

    multilevel game

--- constants.py
+++ constants.py
@@ -4,3 +4,12 @@ if __isDebug:
     url = "http://localhost:8000"
 else:
     url = ""
+    
+__level = 0
+def getLevel():
+    global __level
+    return __level
+
+def setLevel(l):
+    global __level
+    __level = l
