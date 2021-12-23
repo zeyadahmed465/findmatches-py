6f36254a9177f2ee1ce2fa65d76b4858abfeb450
hossam ali <65079183+hossam-ali-shaalan@users.noreply.github.com>
2021-12-23 21:07:00 +0200

    new ux and add retrun to menu

--- findMatches.py
+++ findMatches.py
@@ -7,9 +7,10 @@ level = getLevel()
 
 root = Tk()
 root.geometry("500x{}".format(level * 40 + 150))
+root.config(background="#102027")
 
-if level > 10:
-    level = 10
+if level > 14:
+    level = 14
 elif level % 2:
     level -=1
 
@@ -18,11 +19,23 @@ elif level % 2:
 
 global winner, matches, tries
 
+
+#def recent_scores(info):
+#    scores_list.insert(END, info)
+    
+
+def score_calc():
+    global winner, tries,m , s
+    return  int( 1000 * level * winner / (tries * (s + (m)*60)) )
+
+def time_taken():
+    global m, s
+    return (s + (m)*60)
+
 winner, tries = 0,0
 back_photo = PhotoImage(file="img/back.png")
 
 matches = [PhotoImage(file=f"img/pic{i}.png") for i in range(1,level +1)] * 2
-#matches = [photo1,photo2,photo3,photo4,photo5,photo6]*2
 random.shuffle(matches)
 
 my_frame = Frame(root)
@@ -50,18 +63,22 @@ answer_dict = {}
 m,s,stop = 0,0,0
 time1 = int(time.strftime("%H"))*3600 + int(time.strftime("%M")) *60 + int(time.strftime("%S"))
 #reset the game
+def retrun2menu():
+    root.destroy()
+    import menu
 def reset():
     global matches, winner, stop, time1, tries
     winner =0
     tries = 0
-    my_label2.config(text="00:00")
-    my_label3.config(text="correct : 0")
-    my_label4.config(text="tries : 0")
+    time_label.config(text="00:00")
+    correct_label.config(text="correct : 0")
+    tries_label.config(text="tries : 0")
     #reset our tiles 
     
     for button in buttonList:
         button.config(image= back_photo, state="normal")
     time1 = int(time.strftime("%H"))*3600 + int(time.strftime("%M")) *60 + int(time.strftime("%S"))
+    random.shuffle(matches)
     stop = 0
     clock()
     
@@ -70,17 +87,15 @@ def reset():
 def clock():
     if stop!= 0:
         return 
-    global time1
+    global time1, m, s
     current_time = int(time.strftime("%H"))*3600 + int(time.strftime("%M")) *60 + int(time.strftime("%S"))
     current_time = current_time - time1
     s = current_time % 60
     m = current_time // 60
-    my_label2.config(text="{:02d}:{:02d}".format(m,s))
-    my_label2.after(1000, clock)
+    time_label.config(text="{:02d}:{:02d}".format(m,s))
+    time_label.after(1000, clock)
 
 #creating the winner function
-def win():
-    my_label.config(text="congrates! you win!!")
     
 
 #function for clicking buttons
@@ -99,7 +114,7 @@ def button_click(b, number):
     #start to determine correct or not
     if len(answer_list) == 2:
         tries += 1
-        my_label4.config(text=f"tries : {tries}")
+        tries_label.config(text=f"tries : {tries}")
         keys = list(answer_dict.keys())
 
         if len(keys) == 2 and matches[answer_list[0]] == matches[answer_list[1]]:
@@ -113,10 +128,13 @@ def button_click(b, number):
             count = 0
             #increment our winner
             winner += 1 
-            my_label3.config(text=f"correct : {winner}" )
-            if winner == 6:
-                win()
+            correct_label.config(text=f"correct : {winner}" )
+            if winner == len(buttonList)/2:
                 stop = 1
+                clock()
+                from popUp import pop
+                pop()
+                #win()
         else:
             #my_label.config(text="try again!")
             answer_list = []
@@ -149,19 +167,20 @@ for row in range(rows):
         buttonList[current].configure(command=
                                         lambda current = current: button_click(buttonList[current], current))
 
-my_label = Label(root, text="")
-my_label2 = Label(root, text="", bg="black", fg="white")
-my_label2.after(0, clock)
-my_label2.pack(pady=5)
+
+time_label = Label(root, text="", bg="black", fg="white")
+time_label.after(0, clock)
+time_label.pack(pady=5)
 
 #number of coorect tries label
-my_label4 = Label(root, text="tries : 0", fg="white", bg="black")
-my_label4.pack(pady=5)
-my_label3 = Label(root, text="correct : 0", fg="white", bg="black")
-my_label3.pack(pady=0)
-dummyLabel = Label(root, text= "")
+tries_label = Label(root, text="tries : 0", fg="white", bg="black")
+tries_label.pack(pady=5)
+correct_label = Label(root, text="correct : 0", fg="white", bg="black")
+correct_label.pack(pady=0)
+
+dummyLabel = Label(root, text= "",bg="#102027")
 dummyLabel.pack()
-#number of tries
+
 
 #create a menu
 my_menu = Menu(root)
@@ -172,7 +191,8 @@ option_menu = Menu(my_menu, tearoff=False)
 my_menu.add_cascade(label="options", menu=option_menu)
 my_menu.add_command(label="Reset game",command=reset)
 my_menu.add_separator()
-my_menu.add_command(label="Exit game", command=root.quit)
+my_menu.add_command(label="menu",command=retrun2menu)
+my_menu.add_command(label="Exit game", command=root.destroy)
 
 
 root.mainloop()
--- popUp.py
+++ popUp.py
@@ -2,50 +2,76 @@
 import tkinter
 from tkinter import *
 from tkinter import messagebox
+from findMatches import score_calc, reset
 
-root=Tk()
-root.title("calculator")
+from Score import Score
+
+
+#root=Tk()
+#root.title("calculator")
+
 def sent():
-    pass
+    global e
+    from Score import Score
+    from constants import getLevel
+    from findMatches import time_taken
+    name = e.get()
+    score1 = Score(name,time_taken(),getLevel())
+    score1.storeScore()
+    messagebox.showinfo("Message", "Your result has been saved!")
+    top.destroy()
+    reset()
+    
+    
+    
+
+
 def yes(yesB, noB, l):
+    global e
     yesB.destroy()
     noB.destroy()
     noB.destroy()
 
-    e=Entry(top)
+    e=Entry(top,bg="#102027",fg="white")
     e.grid(row=5,column=1,columnspan=2,padx=10,pady=5)
-    l1=Label(top,text="Enter you name")
+    l1=Label(top,text="Enter you name",bg="#102027",fg="white")
     l1.grid(row=4,column=1,columnspan=2,padx=10,pady=5)
-    submit=Button(top,text="submit",font=("Arial",20),command=sent)
+    submit=Button(top,text="submit",bg="#102027",fg="white",font=("Arial",20),command=sent)
     submit.grid(row=6,column=1,columnspan=2,padx=10,pady=5)
+
 
 def no() :
     top.destroy()
-    root.destroy()
+    #root.destroy()
+
+
+    
 
 def pop():
-    score=1000-500 #must be modified
+    
+    score= score_calc()
     global top
     global yes_button
     global no_button
     top =Toplevel()
     top.title("well done you win !!")
     top.geometry("300x350")
-    l1=Label(top,text=f"Congratulations you got {score} ",font=("Arial",14))
+    top.config(background="#102027")
+    l1=Label(top,text=f"Congratulations you got {score} ",font=("Arial",14),bg="#102027" ,fg="white")
     l1.grid(row=1,column=1,columnspan=2,padx=10,pady=5)
-    label_cong=Label(top,text="Do you want to save this score",font=("Arial",14))
+    label_cong=Label(top,text="Do you want to save this score",font=("Arial",14),bg="#102027",fg="white")
     label_cong.grid(row=2,column=1,columnspan=2,padx=10,pady=5)
-    yes_button=Button(top,text="yes",command=lambda: yes(yes_button, no_button, label_cong),width=10,font=("Arial",10))
+    yes_button=Button(top,text="yes",bg="#102027",fg="white",command=lambda: yes(yes_button, no_button, label_cong),width=10,font=("Arial",10))
     yes_button.grid(row=3,column=1,padx=10,pady=5)
-    no_button= Button(top, text="no", command=no,width=10,font=("Arial",10))
+    no_button= Button(top, text="no",bg="#102027",fg="white", command=no,width=10,font=("Arial",10))
     no_button.grid(row=3, column=2,padx=10,pady=5)
 
 
 
 
 
-b1=Button(root,text="click",command=pop)
-b1.pack()
+# b1=Button(root,text="click",command=pop)
+# b1.pack()
 
 
-mainloop()
+#mainloop()
