6e2118565225a2b4b7b7aed0614b188b10168a13
zeyadahmed465 <64763525+zeyadahmed465@users.noreply.github.com>
2021-12-29 08:28:52 +0200

    Add files via upload

---
+++ menuscore.py
@@ -0,0 +1,46 @@
+from tkinter import *
+from Score import Score
+from tkinter import ttk
+from stylecombo import*
+root = Tk()
+root.geometry("400x500")
+
+
+levelll=gett()
+results = Score.getLevel(levelll)
+
+
+mainFrame = Frame(root)
+mainFrame.pack(fill = BOTH, expand=1)
+
+canvas = Canvas(mainFrame)
+canvas.pack(side=LEFT, fill=BOTH, expand=1)
+
+scrollbar = Scrollbar(mainFrame, orient=VERTICAL, command=canvas.yview)
+scrollbar.pack(side = RIGHT,fill=Y)
+
+canvas.configure(yscrollcommand=scrollbar.set)
+
+canvas.bind('<Configure>', lambda event: 
+    canvas.configure(scrollregion= canvas.bbox(ALL) ))
+
+frame = Frame(canvas)
+
+canvas.create_window((0,0), window=frame, anchor=NW)
+
+texts = [StringVar(frame,"\n".join([str(i[0]).capitalize()+": "+str(i[1]) for i in result.items()]))
+         for result in results
+         ]
+
+labels = [Label(frame, width=50, height=5, background="red", textvariable=text)
+          for text in texts]
+
+for i in range(len(labels)):
+    labels[i].grid(row=i, pady=5, padx=5)
+
+
+
+root.mainloop()
+
+
+#print()
